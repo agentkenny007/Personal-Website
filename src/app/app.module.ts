2b8bfce77c5630ce372c22f2ee26beb8fac41ea4
Ikenna <ikennaugwuh@gmail.com>
2017-02-14 01:48:09 -0500

    Moved import for home page component to the top for consistency.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { BlogComponent } from './blog/blog.component';
 import { ContactComponent } from './contact/contact.component';
@@ -11,7 +12,6 @@ import { HireComponent } from './hire/hire.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { SamplesComponent } from './samples/samples.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
