b223bf431c52c3074aaf7f4275b9818c9ee5ed0e
Ikenna <ikennaugwuh@gmail.com>
2017-02-15 13:35:32 -0500

    Switched from css to scss.

--- src/app/hire/hire.component.ts
+++ src/app/hire/hire.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hire',
   templateUrl: './hire.component.html',
-  styleUrls: ['./hire.component.css']
+  styleUrls: ['./hire.component.scss']
 })
 export class HireComponent implements OnInit {
 
--- src/app/samples/samples.component.ts
+++ src/app/samples/samples.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-samples',
   templateUrl: './samples.component.html',
-  styleUrls: ['./samples.component.css']
+  styleUrls: ['./samples.component.scss']
 })
 export class SamplesComponent implements OnInit {
 
