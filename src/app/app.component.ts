7bd0af3e3f962586b0a6aa5de4dee769a4c0279c
Ikenna <ikennaugwuh@gmail.com>
2017-02-18 20:24:41 -0500

    Set up togglable full screen nav menu.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'root',
@@ -9,4 +10,38 @@ import { Component, ViewEncapsulation } from '@angular/core';
 export class AppComponent {
   navActive = false;
   navBarActive = false;
+
+  makeChecks(grid: Array<number>): string {
+    let board = $('<figure>'), row = $('<div>'), check = $('<span>');
+
+    for (let i = 0, l = grid[1]; i < l; i++) {
+      let o = true, y = row.clone();
+      i % 2 == 0 ? y.addClass('o') : y.addClass('e');
+      for (let j = 0, k = grid[0]; j < k; j++) {
+        let x = check.clone();
+        if (i == 0 && j == 0){} else o ? x.addClass('o') : x.addClass('e'); o = !o;
+        y.append(x);
+      }
+      board.append(y);
+    }
+    return board.html();
+  }
+
+  spell(message: string, elem: any): void {
+
+  }
+
+  openMenu(): void {
+    let w = $(window).width(), h = $(window).height(), c = w < 800 ? 40 : 72,
+        grid = [Math.floor(w/c), Math.ceil(h/c)], v = c * grid[1];
+
+    $('.nav-menu')
+      .show().find('.checks')
+        .css("height", v)
+        .append(this.makeChecks(grid));
+  }
+
+  closeMenu(): void {
+    $('.nav-menu').hide().find('.checks').empty();
+  }
 }
