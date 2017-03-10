ef3f979568d36d12f7f16eeab96d5da78c29da10
Ikenna <ikennaugwuh@gmail.com>
2017-03-10 03:32:22 -0500

    Implemented home page slider.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  flipped: boolean = false;
+  interval: number = 0;
+  playing: boolean = true;
+
+  constructor() {
+    window.setTimeout(function(home){
+      home.flipped = !home.flipped;
+    }, 2000, this);
+    this.playSlider();
+  }
+
+  playSlider(flip?) {
+    if (flip) this.flipped = !this.flipped;
+    this.interval = window.setInterval(function(home){
+      home.flipped = !home.flipped;
+    }, 10000, this);
+  }
+
+  pauseSlider() {
+    if (this.interval) window.clearInterval(this.interval);
+  }
 
   ngOnInit() {
   }
